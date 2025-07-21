a76607c1cb87046074bf5774b172832f1f46cd15
Toasterclan1 <HGCLanson@Gmail.com>
2025-07-21 18:31:03 +1000

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,1 +1,16 @@
+@echo off
+setlocal
+
+set DIR=%~dp0
+set APP_HOME=%DIR%
+
+set JAVA_EXE=java
+
+if defined JAVA_HOME (
+    set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+)
+
+"%JAVA_EXE%" -Xmx64m -cp "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
+
+endlocal
 
